56e9ab66b2c6db0327d3507d3f1260e8315054f2
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-01-12 08:38:15 -0800

    Helper function to generate a random number without duplicates

---
+++ src/helper.js
@@ -0,0 +1,12 @@
+function shuffleArray(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
+
+export default function returnRandomNum() {
+  let arr = [...Array(90).keys()];
+  return shuffleArray(arr).pop();
+}
